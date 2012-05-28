38cf660751512ebc053b92400da491c5f4f2a493
Yaroslav Sivakov <yar3333@gmail.com>
2012-05-28 15:49:05 +0400

    * minor (racing bug)

--- library/haxe/htmlparser/HtmlParser.hx
+++ library/haxe/htmlparser/HtmlParser.hx
@@ -22,7 +22,7 @@ private typedef HtmlLexem =
 class HtmlParser
 {
     public static var selfClosingTags = { img:1, br:1, input:1, meta:1, link:1, hr:1, base:1, embed:1, spacer:1, innercontent:1 };
-    static var regExpForID = '[a-z](?:-?[_a-z0-9])*';
+    static var inline regExpForID = '[a-z](?:-?[_a-z0-9])*';
 
     static public function parse(str:String) : Array<HtmlNode>
     {
