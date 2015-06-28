1cc47740542610598afca487920646275eb2e815
Yaroslav Sivakov <yar3333@gmail.com>
2015-06-29 00:31:48 +0300

    + XmlBuilder: optional beauty output

--- library/htmlparser/XmlBuilder.hx
+++ library/htmlparser/XmlBuilder.hx
@@ -2,16 +2,25 @@ package htmlparser;
 
 class XmlBuilder
 {
+	var indent : String;
 	var cur : XmlNodeElement;
+	var level = 0;
 	public var xml : XmlDocument;
 	
-	public function new()
+	public function new(indent="\t")
 	{
+		this.indent = indent;
 		cur = xml = new XmlDocument();
 	}
 	
 	public function begin(tag:String, ?attrs:Array<{ name:String, value:Dynamic }>) : XmlBuilder
 	{
+		if (indent != null)
+		{
+			if (level > 0) cur.addChild(new XmlNodeText("\n" + StringTools.rpad("", indent, level * indent.length)));
+			level++;
+		}
+		
 		var node = new XmlNodeElement(tag, attrs != null ? attrs.map(function(a) return new XmlAttribute(a.name, a.value, '"')) : []);
 		cur.addChild(node);
 		cur = node;
@@ -20,6 +29,12 @@ class XmlBuilder
 	
 	public function end() : XmlBuilder
 	{
+		if (indent != null)
+		{
+			level--;
+			if (cur.nodes.length > 0) cur.addChild(new XmlNodeText("\n" + StringTools.rpad("", indent, level * indent.length)));
+		}
+		
 		cur = (cast cur.parent : XmlNodeElement);
 		return this;
 	}
